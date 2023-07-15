d5657ff3d4c642b21f00ca29e5331d364a7d36c5
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-07-14 17:47:24 -0700

    a

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,18 +11,15 @@ int i, j;
 
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j < 10; j++)
-{
-if (i != j)
+for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
 
-if (i < 8)
+if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }

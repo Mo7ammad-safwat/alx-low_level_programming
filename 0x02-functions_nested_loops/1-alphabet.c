54a5811da845eed27742580c1e75065c6d34f886
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-07-18 16:27:30 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code . print_alphabet - Prints the alphabet
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@ void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
- _putchar(c);
+_putchar(c);
 _putchar('\n');
 }
+

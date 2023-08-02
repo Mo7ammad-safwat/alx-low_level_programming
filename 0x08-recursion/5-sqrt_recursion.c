a11466a7c60f4292f5bdcf0cb5f3a4cba436bebd
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-08-02 15:40:32 -0700

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int _sqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: natural square root of n
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (_sqrt(n, 0));
+}
+
+/**
+ * _sqrt - helper function to find the square root of a number
+ * @n: number to find the square root of
+ * @i: iterator
+ *
+ * Return: natural square root of n
+ */
+int _sqrt(int n, int i)
+{
+if (i * i == n)
+return (i);
+else if (i * i > n)
+return (-1);
+else
+return (_sqrt(n, i + 1));
+}
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int is_prime_helper(int n, int i);
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: number to check
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+else
+return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - helper function to check if a number is prime
+ * @n: number to check
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, otherwise 0
+ */
+int is_prime_helper(int n, int i)
+{
+if (i > n / 2)
+return (1);
+else if (n % i == 0)
+return (0);
+else
+return (is_prime_helper(n, i + 1));
+}

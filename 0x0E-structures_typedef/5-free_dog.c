4a3c6feb3c3a7d39af3e8570cef39f37277624f8
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-08-14 17:16:10 -0700

    the fnshd task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees a dog
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+
+free(d->name);
+free(d->owner);
+free(d);
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
+
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: owner's name
- *
- * Description: just a lone dog struct in a big kitty world
+ * struct dog - a dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -15,5 +14,24 @@ char *name;
 float age;
 char *owner;
 };
+
+/* define a new type dog_t as a new name for the type struct dog */
+typedef struct dog dog_t;
+
+/**
+ * main - main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+dog_t my_dog;
+
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dog.owner = "Bob";
+printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+return (0);
+}
 #endif
 

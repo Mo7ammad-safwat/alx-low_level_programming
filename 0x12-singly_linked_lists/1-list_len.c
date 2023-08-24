b089ec13f8200e534365fadeacebc061e64e8118
Mo7ammad-safwat <MohammedGhaffar20648@azhar.edu.eg>
2023-08-24 15:33:38 -0700

    a

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the list
- *
- * Return: the number of nodes
+ * list_len - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
  */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
+	int i = 0;
 
-while (h)
-{
-h = h->next;
-count++;
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
-return (count);
-}
-
